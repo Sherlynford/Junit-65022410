a0fbc545745dd95d4544750415ab7e64a1a7644d
Chatchai Sorapin <118619446+Sherlynford@users.noreply.github.com>
2023-12-18 10:42:49 +0700

    Done

---
+++ test/QuizOneJunitTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class QuizOneJunitTest {
+
+	 private QuizOneJunit quiz;
+	   @Before
+	   public void setUp() {
+	       quiz = new QuizOneJunit();
+	   }
+	   @Test
+	   public void testCountLetterA() {
+	       Assert.assertEquals(0, quiz.countLetterA("school"));
+	       Assert.assertEquals(3, quiz.countLetterA("banana"));
+	   }
+	   @Test
+	   public void testCheckTwoLetter() {
+	       Assert.assertTrue(quiz.checkTwoLetter("Ga"));
+	       Assert.assertFalse(quiz.checkTwoLetter("T"));
+	       Assert.assertTrue(quiz.checkTwoLetter("TaTa"));
+	   }
+
+}
